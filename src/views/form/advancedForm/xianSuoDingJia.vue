7dbdff5f3c14b2a3422956f72bbaf7497c54b5a4
zyliang119 <zy_liang@yeah.net>
2020-12-21 16:32:24 +0800

    No.6 upload from c

--- src/views/form/advancedForm/xianSuoDingJia.vue
+++ src/views/form/advancedForm/xianSuoDingJia.vue
@@ -4,11 +4,11 @@
       <a-form :form="form" class="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" @submit="handleSubmit">
         <a-row class="form-row" :gutter="[24,16]">
           <a-col :lg="4" :sm="24">
-              <a-form-item>
-                <a-button type="primary">
-                  <a-icon type="plus" /> 添加定价方案
-                </a-button>
-              </a-form-item>
+            <a-form-item>
+              <a-button type="primary">
+                <a-icon type="plus" /> 添加定价方案
+              </a-button>
+            </a-form-item>
           </a-col>
           <a-col :lg="4" :sm="24">
             <a-form-item
@@ -21,33 +21,71 @@
             </a-form-item>
           </a-col>
           <a-col :lg="8" :sm="24">
-              <a-form-item label="仓库名">
-                <a-input
-                    placeholder="请输入仓库名称"
-                    v-decorator="[
-                    'name',
-                    {rules: [{ required: true, message: '请输入仓库名称', whitespace: true}]}
-                    ]" />
-                </a-form-item>
-          </a-col>
-          <a-col :lg="8" :sm="24">
-            <a-form-item>
-              <a-space>
-                <a-button type="primary">查&nbsp;询</a-button>
-                <a-button>重&nbsp;置</a-button>
-              </a-space>
+            <a-form-item label="仓库名">
+              <a-input
+                  placeholder="请输入仓库名称"
+                  v-decorator="[
+                  'name',
+                  {rules: [{ required: true, message: '请输入仓库名称', whitespace: true}]}
+                  ]" />
             </a-form-item>
+          </a-col>
+          <a-col :lg="8" :sm="24" :style="{ textAlign: 'right' }">
+            <a-button type="primary" :style="{ marginRight:'8px'}">查&nbsp;询</a-button>
+            <a-button :style="{ marginRight:'12px'}">重&nbsp;置</a-button>
           </a-col>
         </a-row>
       </a-form>
+      <a-table :columns="columns" :data-source="tableData">
+      </a-table>
     </a-card>
   </page-header-wrapper>
 </template>
 <script>
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'indexNo',
+    align: 'left',
+    slots: { title: 'ii' }
+  },
+  {
+    title: '方案名称',
+    dataIndex: 'case'
+  },
+  {
+    title: '适用品类',
+    dataIndex: 'cate'
+  },
+  {
+    title: '机型',
+    dataIndex: 'model'
+  },
+  {
+    title: '备注',
+    dataIndex: 'remark'
+  },
+  {
+    title: '状态',
+    dataIndex: 'status'
+  },
+  {
+    title: '更新时间',
+    dataIndex: 'updateTime'
+  },
+  {
+    title: '操作',
+    dataIndex: ''
+  }
+]
+const tableData = []
 export default {
   name: 'XianSuoDingJia',
   data () {
-    return {}
+    return {
+      columns,
+      tableData
+    }
   },
   methods: {
     // handler
